da67a49567573e52691ac9a0a73644c21edca4df
IiroP <iiro.poutiainen@gmail.com>
2025-11-21 20:24:22 +0200

    Day 24 part 2

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use aoc2024::common::read_input;
 use itertools::Itertools;
@@ -114,74 +114,179 @@ fn evaluate_all(gates: &Vec<Gate>, map: &mut HashMap<[char; 3], bool>) {
     }
 }
 
-fn find_broken_gates(gates: &Vec<Gate>, map: &HashMap<[char; 3], bool>) -> Vec<usize> {
-    let mut broken = Vec::new();
-
-    fn is_valid_xy(gate: &Gate, id: [char; 2]) -> bool {
-        (gate.left == ['y', id[0], id[1]] && gate.right == ['x', id[0], id[1]])
-            || (gate.left == ['x', id[0], id[1]] && gate.right == ['y', id[0], id[1]])
-    }
-
-    let mut prev_mem: [char; 3] = ['0', '0', '0'];
-    let mut x_prev: [char; 3] = ['0', '0', '0'];
-
-    let n = map.iter().filter(|(k, _)| k[0] == 'x').count();
-    for i in 1..n {
-        let id: [char; 2] = format!("{:02}", i)
-            .chars()
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
-        let target: [char; 3] = ['z', id[0], id[1]];
-        println!("Checking {:?}", target);
-        let gate = gates.iter().find(|g| g.target == target).unwrap();
-
-        // Find current XOR gate
-        let x_curr = gates
-            .iter()
-            .find(|&g| {
-                g.cmd == 'X' && (g.left == ['x', id[0], id[1]] || g.right == ['x', id[0], id[1]])
-            })
-            .unwrap();
-
-        // Skip if the gate is the first one
-        if gate.left[0] == 'x'
-            || gate.left[0] == 'y'
-            || gate.right[0] == 'x'
-            || gate.right[0] == 'y'
-        {
-            x_prev = x_curr.target;
-            println!("Skipping");
-            continue;
+// Name gates based on the structure of ripple-carry adder
+fn name_gates(gates: &Vec<Gate>) -> HashMap<[char; 3], String> {
+    let mut names = HashMap::new();
+    gates.iter().for_each(|g| {
+        if g.left[0] == 'x' || g.left[0] == 'y' {
+            let index = g.left[1..]
+                .iter()
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+            names.insert(g.target, format!("{}{:02}", g.cmd, index));
         }
+    });
 
-        let prev_id: [char; 2] = format!("{:02}", i - 1)
-            .chars()
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
-
-        // Find other relevant gates
-        let mem = gates
-            .iter()
-            .find(|&g| {
-                g.cmd == 'A'
-                    && (g.left == ['x', prev_id[0], prev_id[1]]
-                        || g.right == ['x', prev_id[0], prev_id[1]])
-            })
-            .unwrap();
-
-        let left = gates.iter().find(|g| g.target == gate.left).unwrap();
-        let right = gates.iter().find(|g| g.target == gate.right).unwrap();
-        if gate.cmd != 'X' {
-            println!("Expected XOR");
-            broken.push(i);
-        } else if left != x_curr && right != x_curr {
-            println!("Fail at: {:?}", gate);
-            broken.push(i);
+    fn iterate_gate(g: &Gate, names: &mut HashMap<[char; 3], String>) {
+        let left = names.get(&g.left).map_or("---", |v| v);
+        let right = names.get(&g.right).map_or("---", |v| v);
+        if g.cmd == 'O' {
+            if left.starts_with('A') {
+                names.insert(g.target, format!("c{:02}", &left[1..]));
+            } else if right.starts_with('A') {
+                names.insert(g.target, format!("c{:02}", &right[1..]));
+            } else if left.starts_with('d') {
+                names.insert(g.target, format!("c{:02}", &left[1..]));
+            } else if right.starts_with('d') {
+                names.insert(g.target, format!("c{:02}", &right[1..]));
+            }
+        } else if g.cmd == 'A' {
+            if left.starts_with('X') {
+                names.insert(g.target, format!("d{:02}", &left[1..]));
+            } else if right.starts_with('X') {
+                names.insert(g.target, format!("d{:02}", &right[1..]));
+            } else if left.starts_with('c') {
+                names.insert(
+                    g.target,
+                    format!("d{:02}", &left[1..].parse::<usize>().unwrap() + 1),
+                );
+            } else if right.starts_with('c') {
+                names.insert(
+                    g.target,
+                    format!("d{:02}", &right[1..].parse::<usize>().unwrap() + 1),
+                );
+            }
+        } else if g.cmd == 'X' {
+            if right.starts_with('X') {
+                names.insert(g.target, format!("z{:02}", &right[1..]));
+            } else if left.starts_with('X') {
+                names.insert(g.target, format!("z{:02}", &left[1..]));
+            } else if (left == "A00" && right == "X01") || (right == "A00" && left == "X01") {
+                names.insert(g.target, "z01".to_string());
+            } else if left.starts_with('c') {
+                names.insert(
+                    g.target,
+                    format!("z{:02}", &left[1..].parse::<usize>().unwrap() + 1),
+                );
+            } else if right.starts_with('c') {
+                names.insert(
+                    g.target,
+                    format!("z{:02}", &right[1..].parse::<usize>().unwrap() + 1),
+                );
+            }
         }
     }
-    broken
+
+    // Iterate this multiple times to get new names from previous rounds
+    (0..3).for_each(|_| {
+        gates.iter().for_each(|g| {
+            iterate_gate(g, &mut names);
+        });
+    });
+    names
+}
+
+#[allow(dead_code)]
+fn gate_with_names(gate: &Gate, names: &HashMap<[char; 3], String>) -> String {
+    let og_target = gate.target.iter().collect();
+    let og_left = gate.left.iter().collect();
+    let og_right = gate.right.iter().collect();
+
+    let gate_name = names.get(&gate.target).unwrap_or(&og_target);
+    let left_name = names.get(&gate.left).unwrap_or(&og_left);
+    let right_name = names.get(&gate.right).unwrap_or(&og_right);
+    format!(
+        "Gate {}: {} {} {}, originally {}: {} {} {}",
+        gate_name, left_name, gate.cmd, right_name, og_target, og_left, gate.cmd, og_right
+    )
+}
+
+fn test_gates(input: &(HashMap<[char; 3], bool>, Vec<Gate>), n: usize) -> Vec<String> {
+    let gates = &input.1;
+    let names = name_gates(gates);
+    let mut errors = HashSet::new();
+
+    // Optionally print all gates with names
+    /*gates.iter().for_each(|g| {
+        let gate_desc = gate_with_names(g, &names);
+        //if names.get(&g.target).is_none() {
+        //    println!("Gate has no name: {}", gate_desc);
+        //}
+        println!("{}", gate_desc);
+    });*/
+
+    (0..n).for_each(|i| {
+        let default_name = "".to_string();
+        let real_name = names
+            .get(&[
+                'z',
+                format!("{:02}", i).chars().nth(0).unwrap(),
+                format!("{:02}", i).chars().nth(1).unwrap(),
+            ])
+            .unwrap_or(&default_name);
+
+        // Test z gate names
+        if real_name.len() > 0 && i > 0 && format!("z{:02}", i) != *real_name {
+            println!("Gate z{:02} has unexpected name {}", i, real_name);
+            errors.insert(format!("z{:02}", i));
+            errors.insert(real_name.clone());
+        }
+
+        // Validate c__ gate
+        let c_gate = gates.iter().find(|g| {
+            let left = names.get(&g.left);
+            let right = names.get(&g.right);
+            g.cmd == 'O'
+                && (left == Some(&format!("A{:02}", i))
+                    || right == Some(&format!("A{:02}", i))
+                    || left == Some(&format!("d{:02}", i))
+                    || right == Some(&format!("d{:02}", i)))
+        });
+        if let Some(c) = c_gate {
+            let default = "---".to_string();
+            let left = names.get(&c.left).unwrap_or(&default);
+            let right = names.get(&c.right).unwrap_or(&default);
+            if (left == &format!("A{:02}", i) && right == &format!("d{:02}", i))
+                || (right == &format!("A{:02}", i) && left == &format!("d{:02}", i))
+            {
+                // Valid
+            } else if left == &format!("A{:02}", i) {
+                // A__ is valid, d__ is missing
+                let missing = format!("d{:02}", i);
+                errors.insert(right.clone());
+                errors.insert(missing);
+            } else if left == &format!("d{:02}", i) {
+                // d__ is valid, A__ is missing
+                let missing = format!("A{:02}", i);
+                errors.insert(right.clone());
+                errors.insert(missing);
+            } else if right == &format!("A{:02}", i) {
+                // A__ is valid, d__ is missing
+                let missing = format!("d{:02}", i);
+                errors.insert(left.clone());
+                errors.insert(missing);
+            } else if right == &format!("d{:02}", i) {
+                // d__ is valid, A__ is missing
+                let missing = format!("A{:02}", i);
+                errors.insert(left.clone());
+                errors.insert(missing);
+            }
+        }
+    });
+
+    let mut result = errors
+        .into_iter()
+        .map(|a| {
+            names
+                .iter()
+                .find(|(_, v)| **v == *a)
+                .map(|(k, _)| k.iter().collect::<String>())
+                .unwrap_or("---".to_string())
+        })
+        .collect_vec();
+    result.sort();
+    result
 }
 
 fn part1(input: &(HashMap<[char; 3], bool>, Vec<Gate>)) -> usize {
@@ -195,53 +300,9 @@ fn part1(input: &(HashMap<[char; 3], bool>, Vec<Gate>)) -> usize {
 }
 
 fn part2(input: &(HashMap<[char; 3], bool>, Vec<Gate>), test: bool) -> String {
-    let map = input.0.clone();
-    let gates = &input.1;
-
-    let gate_indices = (0..gates.len()).collect::<Vec<_>>();
-    let swaps = if test { 4 } else { 8 };
-    let mut possibilities = gate_indices.iter().copied().combinations(swaps);
-
-    // Try all combinations of 4 gates
-    let mut gates = gates.clone();
-    let result = possibilities.find(|p| {
-        // Calculate all permutations = different orders
-        //println!("Testing {:?}", p);
-        p.iter().permutations(p.len()).unique().any(|p1| {
-            println!("Perm: {:?}", p1);
-            gates = gates.clone();
-            let mut map = map.clone();
-
-            // Apply changes
-            p1.windows(2).for_each(|v| {
-                let a = v[0];
-                let b = v[1];
-                let target_a = gates[*a].target;
-                let target_b = gates[*b].target;
-                gates[*a].target = target_b;
-                gates[*b].target = target_a;
-            });
-
-            // Evaluate all gates
-            evaluate_all(&gates, &mut map);
-
-            // Check if we have the right result
-            let x = get_number("x", &map);
-            let y = get_number("y", &map);
-            let z = get_number("z", &map);
-            let target = x & y;
-
-            z == target
-        })
-    });
-
-    result
-        .unwrap()
-        .iter()
-        .map(|&a| gates[a].target)
-        .sorted()
-        .map(|v| v.iter().collect::<String>())
-        .join(",")
+    let n = if test { 6 } else { 45 };
+    let result = test_gates(input, n);
+    result.join(",")
 }
 
 #[cfg(test)]
@@ -337,13 +398,6 @@ tnw OR pbm -> gnj"
     }
 
     #[test]
-    fn test_find_broken() {
-        let (map, gates) = day_input();
-        let result = find_broken_gates(&gates, &map);
-        println!("{:?}", result);
-    }
-
-    #[test]
     fn test_part1() {
         let input = sample_input();
         assert_eq!(4, part1(&input));
@@ -356,34 +410,6 @@ tnw OR pbm -> gnj"
     }
 
     #[test]
-    fn test_part2() {
-        let input: Vec<String> = "x00: 0
-x01: 1
-x02: 0
-x03: 1
-x04: 0
-x05: 1
-y00: 0
-y01: 0
-y02: 1
-y03: 1
-y04: 0
-y05: 1
-
-x00 AND y00 -> z05
-x01 AND y01 -> z02
-x02 AND y02 -> z01
-x03 AND y03 -> z03
-x04 AND y04 -> z04
-x05 AND y05 -> z00"
-            .split('\n')
-            .map(|s| s.to_string())
-            .collect();
-        let input = process_input(input);
-        assert_eq!("z00,z01,z02,z05", part2(&input, true));
-    }
-
-    #[test]
     fn real_part1() {
         let input = day_input();
         assert_eq!(61886126253040, part1(&input));
@@ -392,6 +418,6 @@ x05 AND y05 -> z00"
     #[test]
     fn real_part2() {
         let input = day_input();
-        assert_eq!("", part2(&input, false));
+        assert_eq!("fgt,fpq,nqk,pcp,srn,z07,z24,z32", part2(&input, false));
     }
 }
